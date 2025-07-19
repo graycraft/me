416f5e49128eb3ab903bd64dd6563e36717e08ad
Defined Prototype <5364018+graycraft@users.noreply.github.com>
2025-07-19 14:05:11 +0500

    feat(source/index.mjs): entry point for Webpack compilation at frontend

---
+++ source/index.mjs
@@ -0,0 +1,10 @@
+/**
+ * Entry point for Webpack compilation at frontend.
+ *
+ * @see https://webpack.js.org/concepts/entry-points
+ */
+
+import './main.css';
+import Graycraft from './graycraft.mjs';
+
+export default Graycraft;
