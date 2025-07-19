27ca3c9ed6962c1eb1f9d4ec7a33c556c868733f
Defined Prototype <5364018+graycraft@users.noreply.github.com>
2025-07-19 13:49:19 +0500

    feat(webpack.config.ts): minimize CSS and JS

--- webpack.config.ts
+++ webpack.config.ts
@@ -8,38 +8,28 @@
 
 import nodePath from 'node:path';
 
+import CssMinimizerWebpackPlugin from 'css-minimizer-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import TerserWebpackPlugin from 'terser-webpack-plugin';
 import WorkboxWebpackPlugin from 'workbox-webpack-plugin';
 
 import type { Configuration } from 'webpack';
 
 const isProduction = process.env.NODE_ENV === 'production',
-  stylesHandler = 'style-loader',
   config: Configuration = {
     devtool: 'source-map',
-    entry: './source/graycraft.mjs',
-    output: {
-      filename: 'graycraft.umd.js',
-      /** Prevents "Uncaught ReferenceError: self is not defined". */
-      globalObject: 'this',
-      library: {
-        name: 'graycraft',
-        type: 'umd',
-      },
-      path: nodePath.resolve(__dirname, 'public/javascripts'),
-    },
-    /** @see https://webpack.js.org/configuration/plugins */
-    plugins: [],
+    entry: './source/index.mjs',
     module: {
       rules: [
         /** @see https://webpack.js.org/loaders */
         {
           exclude: ['/node_modules/'],
           loader: 'ts-loader',
-          test: /\.(ts|tsx)$/i,
+          test: /\.(mts|ts)$/i,
         },
         {
           test: /\.css$/i,
-          use: [stylesHandler, 'css-loader', 'postcss-loader'],
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
         },
         {
           test: /\.(eot|gif|jpg|png|svg|ttf|woff|woff2)$/i,
@@ -47,8 +37,24 @@ const isProduction = process.env.NODE_ENV === 'production',
         },
       ],
     },
+    optimization: {
+      minimize: true,
+      minimizer: [new CssMinimizerWebpackPlugin(), new TerserWebpackPlugin()],
+    },
+    output: {
+      filename: 'graycraft.umd.js',
+      /** Solves "Uncaught ReferenceError: self is not defined". */
+      globalObject: 'this',
+      library: {
+        name: 'graycraft',
+        type: 'umd',
+      },
+      path: nodePath.resolve(__dirname, 'distribution'),
+    },
+    /** @see https://webpack.js.org/configuration/plugins */
+    plugins: [new MiniCssExtractPlugin()],
     resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx', '...'],
+      extensions: ['.js', '.mjs', '.mts', '.ts', '...'],
     },
   };
 
